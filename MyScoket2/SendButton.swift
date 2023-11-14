a3fe71fef23c095b1e67c5db4404679826cb7b8f
scbaek <scbaek83@gmail.com>
2023-11-15 01:01:26 +0900

    first commit

---
+++ MyScoket2/SendButton.swift
@@ -0,0 +1,18 @@
+//
+//  SendButton.swift
+//  MyScoket2
+//
+//  Created by 백승철 on 11/15/23.
+//
+
+import SwiftUI
+
+struct SendButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SendButton()
+}
